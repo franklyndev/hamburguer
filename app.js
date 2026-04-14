912615c73cd1ada2acb962e9a48133594a4e6f5b
FranklynSantos <franklynsantos451@gmail.com>
2026-04-14 17:18:08 -0300

    adicionando rotas no app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import pedidoRoutes from './routes/pedidoRoutes.js';
+import entregaRoutes from './routes/entregaRoutes.js';
+import produtoRoutes from './routes/produtoRoutes.js';
 import categoriaRoutes from './routes/categoriaRoutes.js';
+import avaliacaoRoutes from './routes/avaliacaoRoutes.js';
+
 const app = express();
 
 const port = 3000;
@@ -11,6 +16,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/categoria',categoriaRoutes);
+app.use('/pedido',pedidoRoutes);
+app.use('/entrega',entregaRoutes);
+app.use('/avaliacao',avaliacaoRoutes);
+app.use('/produto',produtoRoutes);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
